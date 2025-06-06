b63eea653fbe5b3d5e3dc62b5b82a043919e5f13
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-06-06 13:38:50 +0900

    Add attributes to grid

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ for (let i = 0; i < 16; i++) {
   for (let i = 0; i < 16; i++) {
     const grid = document.createElement("div");
     grid.classList.add("grid");
+    grid.setAttribute("style", "color: yellow; background: black; border: 2px solid green; margin: 8px; width: 30px; height: 30px");
     grid.textContent = "test";
     gridContainer.appendChild(grid);
   }
