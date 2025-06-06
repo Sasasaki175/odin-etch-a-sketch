37def0c00c765ea59f5dfcfe91b4c20b61dcfa19
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-06-06 15:02:43 +0900

    Add a button to change Etch a Sketch size Added a button that prompts the user to input a number between 1-100 and change the Etch a Sketch size accordingly.

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 const container = document.querySelector("#container");
+const changeSizeButton = document.querySelector("#change-size-button");
 
-for (let i = 0; i < 16; i++) {
-  const gridContainer = document.createElement("div");
-  gridContainer.classList.add("grid-container");
+let size = 16;
 
-  for (let i = 0; i < 16; i++) {
-    const grid = document.createElement("div");
-    grid.classList.add("grid");
-    grid.setAttribute("style", "color: yellow; background: black; width: 30px; height: 30px");
-    grid.textContent = "test";
-    grid.addEventListener("mouseover", () => {
-      grid.setAttribute("style", "color: red; background: white; width: 30px; height: 30px");
-    });
-    gridContainer.appendChild(grid);
+createEtchASketch(size);
+
+changeSizeButton.addEventListener("click", () => {
+  let userInput = prompt("Input Etch a Sketch size 1-100", "16")
+  let etchASketchSize = Number(userInput);
+  if (etchASketchSize >= 1 && etchASketchSize <= 100 && etchASketchSize != NaN) {
+    while (container.firstChild) {
+      container.firstChild.remove();
+    }
+    createEtchASketch(etchASketchSize);
+  } else {
+    alert(`\"` + userInput + `\"` + " is not a valid input.");
   }
+});
 
-  container.appendChild(gridContainer);
+function createEtchASketch (size) {
+  for (let i = 0; i < size; i++) {
+    const gridContainer = document.createElement("div");
+    gridContainer.classList.add("grid-container");
+
+    for (let i = 0; i < size; i++) {
+      const grid = document.createElement("div");
+      grid.classList.add("grid");
+      grid.setAttribute("style", "color: yellow; background: black; width: 30px; height: 30px");
+      grid.addEventListener("mouseover", () => {
+        grid.setAttribute("style", "color: red; background: white; width: 30px; height: 30px");
+      });
+      gridContainer.appendChild(grid);
+    }
+
+    container.appendChild(gridContainer);
+  }
 }
 
