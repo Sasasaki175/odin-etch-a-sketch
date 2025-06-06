a53e4931f9497ff571669dbc5f62b21c0239ece9
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-06-06 13:51:25 +0900

    Add event listner to change grid color on mouse over Adjusted attributes and added event listner to the grid to act like an etch a sketch.

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ for (let i = 0; i < 16; i++) {
   for (let i = 0; i < 16; i++) {
     const grid = document.createElement("div");
     grid.classList.add("grid");
-    grid.setAttribute("style", "color: yellow; background: black; border: 2px solid green; margin: 8px; width: 30px; height: 30px");
+    grid.setAttribute("style", "color: yellow; background: black; width: 30px; height: 30px");
     grid.textContent = "test";
+    grid.addEventListener("mouseover", () => {
+      grid.setAttribute("style", "color: red; background: white; width: 30px; height: 30px");
+    });
     gridContainer.appendChild(grid);
   }
 
